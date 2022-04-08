4bcf58e5f53cbdf79c6993d7829ebf544fd94fa7
Juliana Madeira <juliwood1980@gmail.com>
2022-04-08 10:14:16 -0300

    user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+    },
+    passwordHash: {
+        type: String,
+        required:true
+    }
+}, {timestamps: true}
+);
+
+module.exports = model('User', userSchema);
