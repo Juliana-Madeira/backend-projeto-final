1599efd50cf7c3d70138851346b034a507eb4cb2
JessicaNonato <jessica.menezs@gmail.com>
2022-04-09 16:15:10 -0300

    again

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,19 +4,60 @@ const Cart = require ('../models/Cart')
 
 const router = Router();
 
-//Create
-router.post("/", async(req, res) => {
-    const newCart = new Order(req.body);
-    const userId= req.user.id
+// Get all Orders
+router.get('/order', async(req,res) => {
+    const{ id } = req.user;
+
     try{
-        const createCart = {...newCart, user: userId}
-        const cart = await Cart.creat(createCart);
-        await User.findByIdAndUpdate( userId, {$push: {cartId: cart._id}})
-        res.status(200).json(savedCart);
-    } catch (err){
+        const order = await Order.findOne({userId: id}).populate({path:'products', populate: { path: 'productId'}})
+        res.status(200).json(order);
+    } catch(err){
         res.status(500).json(err);
     }
 })
+
+
+//Create
+router.post("/order/:productId", async(req, res) => {
+    const { productId } = req.params;
+    const { quantity } = req.body;
+    const userId= req.user.id;
+   try{
+       const order = await Order.findById( userId );
+    if(!order) {
+        const newOrder = await Order.create( userId );
+        const payload = {
+            productId,
+            orderId: newOrder._id,
+            quantity,
+        }
+        const createCart = await Cart.create(payload);
+        const updateCart = await Order.findByIdAndUpdate(newOrder._id, {$push: {products: createCart._id}},{new: true})
+        res.status(201).json(updateCart);
+    } else {
+        const product = await Cart.findById(productId);
+        if(product){
+            const newQuantity = product.quantity + 1;
+            const updateProducts = await Cart.findByIdAndUpdate(product._id, {quantity: newQuantity});
+            res.status(200).json();
+        } else {
+            const payload ={
+                productId,
+                orderId: newOrder._id,
+                quantity
+            }
+            const createProductinCart = await Cart.create(payload);
+            const updateProductinCart = await Order.findByIdAndUpdate( order._id, {$push: {products : createProductinCart._id}},{new: true})
+            res.status(201).json(updateProductinCart)
+        }
+
+    }
+   } catch(err){
+    res.status(500).json(err);
+   }
+})
+
+//EDITAR < - PARAMOS AQUI
 
 //Carrinho do usuário
 router.get('/:cartId', async(req,res) =>{
